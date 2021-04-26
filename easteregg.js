8add9ba9e0ddce3116b24c3030650356705176d5
Prabesh Shrestha <creatorworld329@gmail.com>
2021-04-26 17:52:53 +0545

    lets gooo easter eggs

---
+++ easteregg.js
@@ -0,0 +1,17 @@
+console.log("you've found a easter egg");
+let index = prompt("enter the index between 0 - 5");
+
+switch (index){
+case 1:
+    console.log("hello");
+case 2:
+    console.log("humans");
+case 3:
+    console.log("humans");
+case 4:
+    console.log("bookselves");
+case 5:
+    console.log("calender");
+case 0:
+    console.log("water");
+}
